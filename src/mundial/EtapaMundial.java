6495bf7f6c1067e1cb338a4c2dc65c46d7e442da
Matias Bernal <matrekb@gmail.com>
2022-12-04 18:24:46 -0300

    feat: add etapaMundial class

---
+++ src/mundial/EtapaMundial.java
@@ -0,0 +1,31 @@
+package mundial;
+
+import java.util.ArrayList;
+
+public class EtapaMundial {
+
+    private String descripcionEtapa;
+    private ArrayList<Partido> partidos;
+    private ArrayList<Equipo> equipos;
+
+    EtapaMundial(){
+        this.partidos = new ArrayList<Partido>();
+        this.equipos = new ArrayList<Equipo>();
+    }
+
+    public String getDescripcionEtapa(){
+        return descripcionEtapa;
+    }
+
+    public void addPartido(ArrayList<Partido> partidos){
+        this.partidos = partidos;
+    }
+
+    public ArrayList<Partido> getPartidos(){
+        return partidos;
+    }
+
+    public ArrayList<Equipo> getEquiposQueAvanzan(){
+        return equipos;
+    }
+}
