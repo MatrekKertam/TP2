41a894311790e1787051413b23cb62533a4d51c8
Matias Bernal <matrekb@gmail.com>
2022-12-04 18:50:39 -0300

    feat: add vehiculos class

---
+++ src/garage/Vehiculos.java
@@ -0,0 +1,16 @@
+package garage;
+
+public class Vehiculos {
+
+    private String motor;
+    private int cantidadDeCombustible;
+    private String patente;
+    private String marca;
+    private int ruedas;
+    private int kilometros;
+
+    public String getAviso(){
+        return aviso;
+    }
+
+}
